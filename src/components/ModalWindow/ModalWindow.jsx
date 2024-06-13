a1ab1d972ac37f4d019fffdcbc46e3684c8a9d3d
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-06-13 20:12:50 +0300

    add Loader, ModalWindow component

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import s from "./ModalWindow.module.css";
-
-export const ModalWindow = ({ onHandleCloseModal, onRename, onAddModalValue }) => {
- const [title, setTitle] = useState('')
- const [body, setBody] = useState('')
- const [author, setAuthor] = useState('')
-
+import { IoCloseOutline } from "react-icons/io5";
+import { ParamsList } from "../ParamsList/ParamsList";
+export const ModalWindow = ({ onClose }) => {
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === "Escape") {
-        onHandleCloseModal();
+        onClose();
       }
     };
     document.addEventListener("keydown", handleKeyDown);
@@ -17,22 +14,13 @@ export const ModalWindow = ({ onHandleCloseModal, onRename, onAddModalValue }) =
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [onHandleCloseModal]);
+  }, [onClose]);
 
   const handleClickOutside = (e) => {
     if (e.target === e.currentTarget) {
-      onHandleCloseModal();
+      onClose();
     }
   };
-
-  const handleSave = () => {
-    onRename(inputText);
-    onHandleCloseModal();
-  };
-
-  const handlerAddForm = () => {
-    onAddModalValue({title, body, author})
-  }
 
   return (
     <div
@@ -40,36 +28,62 @@ export const ModalWindow = ({ onHandleCloseModal, onRename, onAddModalValue }) =
       onClick={handleClickOutside}
     >
       <div className={s.modal_content}>
-        <span
-          className={s.close}
-          onClick={onHandleCloseModal}
-        >
-          &times;
-        </span>
-        <input
-          type="text"
-          className={s.input}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter title"
-        />
-        <input
-          type="text"
-          className={s.input}
-          onChange={(e) => setBody(e.target.value)}
-          placeholder="Enter text"
-        />
-        <input
-          type="text"
-          className={s.input}
-          onChange={(e) => setAuthor(e.target.value)}
-          placeholder="Enter author"
-        />
         <button
-          className={s.btn_add}
-          onClick={handlerAddForm}
+          className={s.close}
+          onClick={onClose}
         >
-          Add
+          <IoCloseOutline size={24} />
         </button>
+        <div className={s.img_block}>
+          <img
+            src="https://imageio.forbes.com/specials-images/imageserve/6064b148afc9b47d022718d1/Hennessey-Venom-F5/960x0.jpg?height=473&width=711&fit=bounds"
+            alt=""
+          />
+        </div>
+        <div className={s.title_block}>
+          <h2 className={s.title}>
+            Buick <span className={s.model}> Enclave</span>,
+            <span className={s.year}>2008</span>
+          </h2>
+        </div>
+        <div className={s.tag_block}>
+          <ParamsList />
+        </div>
+
+        <p className={s.description_text}>
+          The Buick Enclave is a stylish and spacious SUV known for its
+          comfortable ride and luxurious features.
+        </p>
+        <div className={s.functional_block}>
+          <h3 className={s.functional_title}>
+            Accessories and functionalities:
+          </h3>
+          <ParamsList />
+        </div>
+        <div className={s.rental_block}>
+          <h3 className={s.rental_title}>Rental Conditions: </h3>
+          <ul className={s.rental_list}>
+            <li className={s.rental_item}>
+              Minimum age: <span>25</span>
+            </li>
+            <li className={s.rental_item}>Valid driver’s license</li>
+            <li className={s.rental_item}>Security deposite required </li>
+            <li className={s.rental_item}>
+              Mileage: <span>5,858</span>
+            </li>
+            <li className={s.rental_item}>
+              Price: <span>40$</span>
+            </li>
+          </ul>
+        </div>
+        <div className={s.link_block}>
+          <a
+            href="tel:+380730000000"
+            className={s.link}
+          >
+            Rental car
+          </a>
+        </div>
       </div>
     </div>
   );
