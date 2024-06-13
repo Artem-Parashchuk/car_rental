79e3867e0f1fdf209738e6914756d028b7f608d5
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-06-13 20:08:45 +0300

    add Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
+import { Loader } from "../../components/Loader/Loader";
+import s from "./Home.module.css";
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={s.container}>
+      <Loader />
+    </div>
+  );
+};
