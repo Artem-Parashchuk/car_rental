ccb7f85ffb61ae08e0da8b4e251a52518b83d517
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-06-15 00:02:36 +0300

    Update Catalog.jsx

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,12 +1,24 @@
 import s from "./Catalog.module.css";
 import { CarList } from "../../components/CarList/CarList";
 import { SearchForm } from "../../components/SearchForm/SearchForm";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCars, selectIsLoading } from "../../redux/slice";
+import { useEffect } from "react";
+import { fetchCarsThunk } from "../../redux/operations";
+import { Loader } from "../../components/Loader/Loader";
 
 export const Catalog = () => {
+  const dispatch = useDispatch();
+  const cars = useSelector(selectCars);
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchCarsThunk());
+  }, [dispatch]);
   return (
     <div className={s.container}>
       <SearchForm />
-      <CarList />
+      {isLoading ? <Loader /> : <CarList cars={cars} />}
     </div>
   );
 };
